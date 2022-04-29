95d3565442b83664b1e795877e5242ceabff2c8b
PatrykKaminski98 <patryk.kaminski@interia.com>
2022-04-29 17:01:41 +0200

    displaying institutions list

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,26 @@
 package pl.coderslab.charity;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.Institution.Institution;
+import pl.coderslab.charity.Institution.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+
+    private InstitutionRepository institutionRepository;
 
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        List<Institution> institutionList = institutionRepository.findAll();
+        model.addAttribute("institutions", institutionList);
         return "index";
     }
 }
