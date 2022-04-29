f81593e4fe643102839924a61fd19486608a95c4
PatrykKaminski98 <patryk.kaminski@interia.com>
2022-04-29 16:22:03 +0200

    add Category(Entity, Repository)

---
+++ src/main/java/pl/coderslab/charity/Category/Category.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.Category;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+public class Category {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String name;
+}
