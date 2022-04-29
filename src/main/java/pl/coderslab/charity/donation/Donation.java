ba8e47f7e8aea9fcdba2aa4ec481467183231f5d
PatrykKaminski98 <patryk.kaminski@interia.com>
2022-04-29 16:34:44 +0200

    add Donation (Entity, Repository)

---
+++ src/main/java/pl/coderslab/charity/donation/Donation.java
@@ -0,0 +1,31 @@
+package pl.coderslab.charity.donation;
+
+import lombok.Getter;
+import lombok.Setter;
+import pl.coderslab.charity.Category.Category;
+import pl.coderslab.charity.Institution.Institution;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+public class Donation {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private int quantity;
+    @OneToMany
+    private List<Category> categories;
+    @OneToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+}
